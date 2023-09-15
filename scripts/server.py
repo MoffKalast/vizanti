8e74bc5061483744d6684aa7611fd513f796c80e
Shih-Wei Guo <tony2guo@gmail.com>
2023-09-15 16:28:05 +0800

    Add base_url

--- scripts/server.py
+++ scripts/server.py
@@ -9,10 +9,16 @@ import json
 from flask import Flask, render_template, send_from_directory, make_response
 from werkzeug.serving import make_server, WSGIRequestHandler
 
+rospy.init_node('vizanti_flask_node')
+
+param_host = rospy.get_param('~host', '0.0.0.0')
+param_port = rospy.get_param('~port', 5000)
+param_base_url = rospy.get_param('~base_url', '')
 
 public_dir = RosPack().get_path('vizanti') + '/public/'
 
 app = Flask(__name__, static_folder=public_dir, template_folder=public_dir)
+app.debug = rospy.get_param('~flask_debug', True)
 
 def get_files(path, valid_extensions):
 	templates_dir = os.path.join(app.static_folder, path)
@@ -48,19 +54,19 @@ def get_paths(path, valid_extensions):
 	response.headers['Content-Type'] = 'application/javascript'
 	return response
 
-@app.route('/')
+@app.route(param_base_url + '/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html', base_url=param_base_url)
 
-@app.route('/templates/files')
+@app.route(param_base_url + '/templates/files')
 def list_template_files():
 	return get_files("templates", ['.html', '.js', '.css'])
 
-@app.route('/assets/robot_model/paths')
+@app.route(param_base_url + '/assets/robot_model/paths')
 def list_robot_model_files():
 	return get_paths("assets/robot_model", ['.png'])
 
-@app.route('/<path:path>')
+@app.route(param_base_url + '/<path:path>')
 def serve_static(path):
 	return send_from_directory(app.static_folder, path)
 
@@ -92,12 +98,6 @@ class ServerThread(threading.Thread):
 	def shutdown(self):
 		self.srv.shutdown()
 
-rospy.init_node('vizanti_flask_node')
-
-param_host = rospy.get_param('~host', '0.0.0.0')
-param_port = rospy.get_param('~port', 5000)
-
-app.debug = rospy.get_param('~flask_debug', True)
 
 server = ServerThread(app, param_host, param_port)
 server.start()
