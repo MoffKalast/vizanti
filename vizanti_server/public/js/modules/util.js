0206d4aab952ea8ad0688812c386d7f89c6a182c
MoffKalast <vid.rijavec@gmail.com>
2024-04-10 16:56:12 +0200

    finalized tf grouping

--- vizanti_server/public/js/modules/util.js
+++ vizanti_server/public/js/modules/util.js
@@ -41,33 +41,82 @@ export function imageToDataURL(url) {
 		img.src = url;
 	});
 }
-
 export function groupStringsByPrefix(strings, minPrefixLength=2) {
 
-	function getPrefix(str, minPrefixLength) {
-		const prefixEnd = str.indexOf('/') === -1 ? str.indexOf('_') : str.indexOf('/');
-		const prefix = prefixEnd === -1 ? str : str.slice(0, prefixEnd);
-		return prefix.length >= minPrefixLength ? prefix : null;
-	}
-
-	const prefixMap = new Map();
-	for (const str of strings) {
-		const prefix = getPrefix(str, minPrefixLength);
-		if (!prefixMap.has(prefix)) {
-			prefixMap.set(prefix, []);
+	function splitPrefix(string_list) {		
+		class Node {
+			constructor() {
+				this.list = {};
+			}
 		}
-		prefixMap.get(prefix).push(str);
+
+		const root = new Node();
+		for (const i of string_list) {
+			const replaced = i.replaceAll("/","@@/")
+							.replaceAll("_","@@_")
+							.replaceAll("-","@@-");
+
+			const path = replaced.split("@@");
+
+			let currentNode = root;
+			for(let j = 0; j < path.length; j++){
+				const segment = path[j];
+				if (!(segment in currentNode.list))
+					currentNode.list[segment] = new Node();
+				currentNode = currentNode.list[segment];
+			}
+
+			currentNode.link_name = i;
+		}		
+		return root;
 	}
 
+	function getLinks(tree){
+		let links = "link_name" in tree ? [tree.link_name] : [];
+
+		for (const [key, value] of Object.entries(tree.list)) {
+			links = links.concat(getLinks(value));
+		}
+
+		return links;
+	}
+
+	function getGroups(tree){
+		let groups = {};
+		for (const [key, value] of Object.entries(tree.list)) {
+			const links = getLinks(value);
+
+			if(links.length < 8){
+				groups[key] = links;
+			}else{
+				let subgroups = getGroups(value);
+				for (const [subkey, subvalue] of Object.entries(subgroups)) {
+					if(subvalue.length > 2){
+						groups[key+subkey] = subvalue;
+					}else{
+						if(key in groups)
+							groups[key] = groups[key].concat(subvalue);
+						else
+							groups[key] = subvalue;
+					}					
+				}
+			}
+		}
+		return groups;
+	}
+
+	let fragmented = splitPrefix(strings);
+	let dict = getGroups(fragmented);
+
+	//format for rendering
 	let result = [];
-	for (const [prefix, group] of prefixMap) {
-		if(group.length == 1){
-			result.push([group[0]]);
-		}else{
-			result.push([prefix, ...group]);
-		}
+	for (const [key, value] of Object.entries(dict)) {
+		if (value.length > 1)
+			result.push([key, ...value])
+		else
+			result.push(value)
 	}
-	 
+
 	return result.sort((a,b) => {
 		if (a[0] < b[0]) return -1;
 		if (a[0] > b[0]) return 1;
