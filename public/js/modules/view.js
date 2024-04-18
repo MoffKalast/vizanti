1a0cf8120631b8197805d104233234c5335528b9
Bartosz Multan <barmulta@pg.edu.pl>
2024-04-18 13:39:03 +0200

    trying to change zoomLevel

--- public/js/modules/view.js
+++ public/js/modules/view.js
@@ -29,7 +29,7 @@ function hasClassInParentChain(element, className) {
 }
 
 const MAX_SCALE = 10000;
-const MIN_SCALE = 3.0;
+const MIN_SCALE = 0.01;
 const ZOOM_FACTOR = 1.05;
 
 export class View {
