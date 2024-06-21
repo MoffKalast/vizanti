dd267bb5b2356c2e476f6fbd641c1d1edc1b3059
MoffKalast <vid.rijavec@gmail.com>
2024-06-21 17:05:49 +0200

    partial altimeter indicator

--- vizanti_server/public/templates/altimeter/altimeter_script.js
+++ vizanti_server/public/templates/altimeter/altimeter_script.js
@@ -17,6 +17,7 @@ let MODES = {
 	"depth_positive": {dir: "depth", invert: false},
 };
 
+let meters = 0;
 let frame = "";
 let topic = getTopic("{uniqueID}");
 let status = new Status(
@@ -24,11 +25,15 @@ let status = new Status(
 	document.getElementById("{uniqueID}_status")
 );
 
+const canvas = document.getElementById('{uniqueID}_canvas');
+const ctx = canvas.getContext('2d', { colorSpace: 'srgb' });
+
 const icon = document.getElementById("{uniqueID}_icon").getElementsByTagName('img')[0];
 const selectionbox = document.getElementById("{uniqueID}_topic");
 const frameSelector = document.getElementById("{uniqueID}_frame");
 const modeSelector = document.getElementById("{uniqueID}_mode");
 const text_altitude = document.getElementById("{uniqueID}_altitude");
+const stepBox = document.getElementById("{uniqueID}_step");
 
 if(settings.hasOwnProperty("{uniqueID}")){
 	const loaded_data  = settings["{uniqueID}"];
@@ -36,6 +41,7 @@ if(settings.hasOwnProperty("{uniqueID}")){
 	frame = loaded_data.frame;
 	topic = loaded_data.topic;
 
+	stepBox.value = loaded_data.step;
 	frameSelector.value = frame;
 	selectionbox.value = topic;
 
@@ -46,6 +52,11 @@ if(settings.hasOwnProperty("{uniqueID}")){
 		status.setWarn("No frame found, defaulting to base_link");
 	}
 
+	if(topic == ""){
+		topic = "/depth_tgt";
+		status.setWarn("No topic found, defaulting to /depth_tgt");
+	}
+
 	saveSettings();
 }
 
@@ -53,7 +64,8 @@ function saveSettings(){
 	settings["{uniqueID}"] = {
 		mode: modeSelector.value,
 		frame: frame,
-		topic: topic
+		topic: topic,
+		step: stepBox.value
 	}
 	settings.save();
 }
@@ -72,40 +84,99 @@ function sendMessage(floatval){
 	publisher.publish(floatMsg);
 }
 
-async function drawWidget() {
-
+function getMeters(){
 	const robotframe = tf.absoluteTransforms[frame];
-	if(robotframe){
-
+	if(robotframe)
+	{
 		const mode = MODES[modeSelector.value];
-		let meters = robotframe.translation.z;
+		let z_meters = robotframe.translation.z;
 
 		if(mode.invert)
-			meters = -meters;
+			z_meters = -z_meters;
 
-		if(meters < 0)
-			meters = 0;
-
-		if(mode.dir === "depth")
-			text_altitude.innerText = "Depth: "+meters.toFixed(3)+" m";
-		else
-			text_altitude.innerText = "Altitude: "+meters.toFixed(3)+" m";
+		if(z_meters < 0)
+			z_meters = 0;
 
 		status.setOK();
-	}else{
+		return parseFloat(z_meters.toFixed(3));
+	}
+	else
+	{
 		status.setError("Required transform frame \""+frame+"\" not found.");
+		return 0;
 	}
 }
 
+async function drawWidget() {
+
+	const mode = MODES[modeSelector.value];
+
+	if(mode.dir === "depth")
+		text_altitude.innerText = "Depth: "+meters.toFixed(3)+" m";
+	else
+		text_altitude.innerText = "Altitude: "+meters.toFixed(3)+" m";
+
+	const icon_offset = document.getElementById("icon_bar").getBoundingClientRect().height;
+	const wid = canvas.width;
+	const hei = canvas.height;
+	const centerY = hei / 2;
+	const step = Math.min(Math.max(0.1, parseFloat(stepBox.value)), 1000);
+
+	const offsetMeters = meters;
+    const pixelOffset = (offsetMeters / step) * -100 + centerY;
+
+	ctx.clearRect(0, 0, wid, hei);
+	
+	ctx.strokeStyle = "lightgray";
+	ctx.lineWidth = 1;
+	ctx.beginPath();
+	for (let y = pixelOffset, x = 0; y <= hei; y += 10, x+=1) {
+		if(x % 5 == 0)
+			continue;
+
+		ctx.moveTo(0, y);
+		ctx.lineTo(25, y);
+	}
+	ctx.stroke();
+
+	ctx.strokeStyle = "white";
+	ctx.lineWidth = 2;
+	ctx.font = "bold 16px Monospace";
+	ctx.fillStyle = "white";
+	
+	ctx.beginPath();
+	let lineCount = 0.0;
+	for (let y = pixelOffset, x = 0; y <= hei; y += 50, x+=1) {
+
+		if(x % 2 == 0){
+			ctx.moveTo(0, y);
+			ctx.lineTo(50, y);
+			
+			ctx.fillText(lineCount.toFixed(1), 55, y + 4);
+			lineCount += step;
+		}else{
+			ctx.moveTo(0, y);
+			ctx.lineTo(30, y);
+		}
+	}
+	ctx.stroke();
+}
+
 function resizeScreen(){
-	//canvas.height = window.innerHeight;
-	//canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	canvas.width = 110;
 	drawWidget();
 }
 
 window.addEventListener("tf_fixed_frame_changed", drawWidget);
-window.addEventListener("tf_changed", drawWidget);
-window.addEventListener("view_changed", drawWidget);
+window.addEventListener("tf_changed", ()=>{
+	let new_val = getMeters();
+	if(new_val != meters){
+		meters = new_val;
+		drawWidget();
+	}
+});
+
 window.addEventListener('resize', resizeScreen);
 window.addEventListener('orientationchange', resizeScreen);
 
@@ -128,12 +199,21 @@ function setFrameList(){
 
 modeSelector.addEventListener("change", (event) => {
 	saveSettings();
+	drawWidget();
 });
 
 frameSelector.addEventListener("change", (event) =>{
 	frame = frameSelector.value;
 	saveSettings();
+	drawWidget();
 });
+
+stepBox.addEventListener("change", (event) =>{	
+	saveSettings();
+	drawWidget();
+});
+
+
 
 // Topics
 async function loadTopics(){
