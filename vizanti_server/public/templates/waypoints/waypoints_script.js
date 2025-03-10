3988329c8186c245e57a4788b3df96d7dfad4aac
MoffKalast <vid.rijavec@gmail.com>
2025-03-10 14:00:45 -0400

    waypoints subscribing to correct type on load fix

--- vizanti_server/public/templates/waypoints/waypoints_script.js
+++ vizanti_server/public/templates/waypoints/waypoints_script.js
@@ -76,6 +76,9 @@ if(settings.hasOwnProperty("{uniqueID}")){
 	margin.value = loaded_data.margin ?? 0.8;
 	startCheckbox.checked = loaded_data.start_closest;
 
+	if(loaded_data.topic_type != undefined)
+		typedict[topic] = loaded_data.topic_type;
+
 	for (let i = 0; i < points.length; i++) {
 		if (points[i].z == null || points[i].z == undefined)
 			points[i].z = 0;
@@ -94,6 +97,7 @@ if(topic == ""){
 function saveSettings(){
 	settings["{uniqueID}"] = {
 		topic: topic,
+		topic_type: typedict[topic],
 		fixed_frame: fixed_frame,
 		base_link_frame: base_link_frame,
 		points: points,
