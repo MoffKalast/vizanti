7728b3da0e64753d15fa1ced55423a843f14b75e
MoffKalast <vid.rijavec@fri.uni-lj.si>
2026-05-06 15:23:44 +0200

    markerarray miter fix part 2

--- vizanti_server/public/templates/markerarray/markerarray_script.js
+++ vizanti_server/public/templates/markerarray/markerarray_script.js
@@ -328,8 +328,9 @@ async function drawMarkers(){
 		}
 
 		// Reset so miter calculation doesn't bug out for other marker types
-		ctx.lineJoin = 'miter';
-		ctx.miterLimit = 10;
+		ctx.lineWidth = 1;                   // Default line width
+		ctx.lineJoin = 'miter';              // Default line join
+		ctx.miterLimit = 10;                 // Default miter limit
 	}
 
 	function drawTriangleList(marker, size) {
